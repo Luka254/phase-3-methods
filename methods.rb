248161376100a66ea858975c2e3ae06f3762cec0
Luke <luke.kazungu@student.moringaschool.com>
2022-11-10 16:16:12 +0300

    Assignment done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("Karisa")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default
+greet_with_default("Karz")
+
+def add(num1, num2)
+    puts num1 + num2
+    num1 + num2
+end
+add(1,2)
+
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+    num / 2 
+end
+puts halve("Luke")
+puts halve(6)
